794ea9639b38e12864197baa97223635e334e415
RafaelH31 <rafael.cebrian@sptech.school>
2023-08-23 23:40:14 -0300

    add(tabelas do banco de dados)

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,117 +1,109 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database test;
+use test;
 
-/*
-comandos para mysql - banco local - ambiente de desenvolvimento
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj VARCHAR(14)
+CREATE TABLE instituicao (
+	idinstituicao INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45),
+	email VARCHAR(45),
+	senha VARCHAR(45),
+    CNPJ char(14),
+    CEP char(9),
+    telefone char(11)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-
-/*
-comando para sql server - banco remoto - ambiente de produção
-*/
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	razao_social VARCHAR(50),
-	cnpj VARCHAR(14)
-);
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT FOREIGN KEY REFERENCES empresa(id)
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY IDENTITY(1,1),
-	descricao VARCHAR(300),
-	fk_empresa INT FOREIGN KEY REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
-);
-
-/*
-comandos para criar usuário em banco de dados azure, sqlserver,
-com permissão de insert + update + delete + select
-*/
-
-CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
-WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
-DEFAULT_SCHEMA = dbo;
-
-EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
-
-EXEC sys.sp_addrolemember @rolename = N'db_datareader',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+CREATE TABLE funcionario (
+	idfuncionario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    email VARCHAR(45),
+	senha VARCHAR(45),
+    nivelAcesso INT,
+    fk_instituicao INT,
+	CONSTRAINT fkinstituicao FOREIGN KEY (fk_instituicao)
+	REFERENCES instituicao(idinstituicao)
+    );
+    
+CREATE TABLE curso (
+	idCurso INT PRIMARY KEY AUTO_INCREMENT,
+    disciplina VARCHAR(45),
+	qtdAlunos INT,
+    fk_instituicao INT,
+	CONSTRAINT fk_instituicao FOREIGN KEY (fk_instituicao)
+	REFERENCES instituicao(idinstituicao)
+    );
+    
+CREATE TABLE computador (
+	idHardware INT PRIMARY KEY AUTO_INCREMENT,
+    CPU VARCHAR(45),
+	memoria VARCHAR(45),
+    disco VARCHAR(45),
+    rede VARCHAR(45),
+    fk_instituicao INT,
+	CONSTRAINT fk_instituicao_computador FOREIGN KEY (fk_instituicao)
+	REFERENCES instituicao(idinstituicao),
+    fk_curso INT,
+	CONSTRAINT fk_curso_computador FOREIGN KEY (fk_curso)
+	REFERENCES curso(idcurso)    
+    );
+    
+CREATE TABLE CPU (
+	idregistro_cpu INT PRIMARY KEY AUTO_INCREMENT,
+    CPU VARCHAR(45),
+	utilizacao FLOAT,
+    velocidade FLOAT,
+    data_hora DATETIME,
+    fk_instituicao INT,
+	CONSTRAINT fk_instituicao_cpu FOREIGN KEY (fk_instituicao)
+	REFERENCES instituicao(idinstituicao),
+    fk_curso INT,
+	CONSTRAINT fk_curso_cpu FOREIGN KEY (fk_curso)
+	REFERENCES curso(idcurso),   
+    fk_computador INT,
+	CONSTRAINT fk_computador_cpu FOREIGN KEY (fk_computador)
+	REFERENCES computador(idhardware)
+    );
+    
+CREATE TABLE memoria (
+	idregistro_memoria INT PRIMARY KEY AUTO_INCREMENT,
+	usada FLOAT,
+    disponivel FLOAT,
+    data_hora DATETIME,
+    fk_instituicao INT,
+	CONSTRAINT fk_instituicao_memoria FOREIGN KEY (fk_instituicao)
+	REFERENCES instituicao(idinstituicao),
+    fk_curso INT,
+	CONSTRAINT fk_curso_memoria FOREIGN KEY (fk_curso)
+	REFERENCES curso(idcurso),   
+    fk_computador INT,
+	CONSTRAINT fk_computador_memoria FOREIGN KEY (fk_computador)
+	REFERENCES computador(idhardware)
+    );
+    
+    CREATE TABLE disco (
+	idregistro_disco INT PRIMARY KEY AUTO_INCREMENT,
+    usado FLOAT,
+    disponivel FLOAT,
+    data_hora DATETIME,
+    fk_instituicao INT,
+	CONSTRAINT fk_instituicao_disco FOREIGN KEY (fk_instituicao)
+	REFERENCES instituicao(idinstituicao),
+    fk_curso INT,
+	CONSTRAINT fk_curso_disco FOREIGN KEY (fk_curso)
+	REFERENCES curso(idcurso),   
+    fk_computador INT,
+	CONSTRAINT fk_computador_disco FOREIGN KEY (fk_computador)
+	REFERENCES computador(idhardware)
+    );
+    
+	CREATE TABLE rede (
+	idregistro_rede INT PRIMARY KEY AUTO_INCREMENT,
+    data_hora DATETIME,
+    fk_instituicao INT,
+	CONSTRAINT fk_instituicao_rede FOREIGN KEY (fk_instituicao)
+	REFERENCES instituicao(idinstituicao),
+    fk_curso INT,
+	CONSTRAINT fk_curso_rede FOREIGN KEY (fk_curso)
+	REFERENCES curso(idcurso),   
+    fk_computador INT,
+	CONSTRAINT fk_computador_rede FOREIGN KEY (fk_computador)
+	REFERENCES computador(idhardware)
+    );
