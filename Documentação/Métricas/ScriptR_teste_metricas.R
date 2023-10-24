12a523eedbcaa11fefc3c0025b357ef17dd12311
bruna.rasmussen@sptech.school <bruna.rasmussen@sptech.school>
2023-10-24 10:52:14 -0300

    edição na nota cruzamento de informações

--- Documentação/Métricas/ScriptR_teste_metricas.R
+++ Documentação/Métricas/ScriptR_teste_metricas.R
@@ -1,4 +1,4 @@
-summary(RegistrosTRUSTED)
+summary(`RegistrosTRUSTED`)
 
 comp <- RegistrosTRUSTED$fkComponente
 
@@ -6,10 +6,24 @@ CPU <- subset(RegistrosTRUSTED,fkComponente == 1)
 RAM<- subset(RegistrosTRUSTED,fkComponente == 2)
 DISCO <- subset(RegistrosTRUSTED,fkComponente == 3)
 LATENCIA <- subset(RegistrosTRUSTED,fkComponente == 4)
+TEMPO <- RegistrosTRUSTED$dataHora
 
 summary(CPU$dadosCapturados)
 summary(RAM$dadosCapturados)
 summary(DISCO$dadosCapturados)
 summary(LATENCIA$dadosCapturados)
 
+  TEMPO
+  CPU
+  RAM
+  DISCO
+  LATENCIA
+  
+  dadosCPU <- c(CPU$dadosCapturados)
+  dadosCPU
+  nrow(dadosCPU)
+  nrow(CPU$dadosCapturados)
+  nrow(TEMPO)
+  
+  plot(TEMPO, dadosCPU, type = "l", col = "blue", xlab = "TEMPO", ylab = "CPU", main = "Gráfico de Linha Simples")
   
